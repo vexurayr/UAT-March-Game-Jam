f239b3c29f2aac9aa65e666a9eaf82b411522e5c
vexurayr <70838146+vexurayr@users.noreply.github.com>
2023-03-04 12:23:10 -0700

    Camera & Player Movement
    The camera now tracks only the player's movement along the x axis and the player can't move so far up or down that they'd be out of camera view.

--- Assets/Scripts/Components/Background.cs
+++ Assets/Scripts/Components/Background.cs
@@ -5,16 +5,4 @@ using UnityEngine;
 public class Background : MonoBehaviour
 {
     public GameObject verticalBorder;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
